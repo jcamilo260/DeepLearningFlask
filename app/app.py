06f8ca1faf92a234f104cf6d6b40c4f78bde5ccd
Juan Camilo <juancamiloarguellesardila@gmail.com>
2022-05-14 12:02:26 -0500

    v1

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+    data={
+       'titulo': 'Index',
+       'bienvenida': '¡Saludos!'
+    }
+    return render_template('index.html', data=data)
+
+@app.route('/acerca/')
+def contacto():
+    return render_template('contacto.html')
+
+if __name__== '__main__':
+    app.run(debug=True, port=5000)
